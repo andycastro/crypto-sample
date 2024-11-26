66adc21d2d24be3cf31f411724bb12a6ce303d05
Andy Castro <contato@andycastro.com.br>
2024-11-26 00:06:52 -0300

    feat: style adjusts

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -8,9 +8,9 @@ type MainLayoutProps = {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen w-full">
       <Header />
-      <main className="flex-grow p-4">{children}</main>
+      <main className="flex-grow p-4 w-full">{children}</main>
       <Footer />
     </div>
   );
