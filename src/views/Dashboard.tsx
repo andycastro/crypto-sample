180cf278f28a08da7758bd1c8187b5ec51b48b08
Andy Castro <contato@andycastro.com.br>
2024-11-26 00:05:39 -0300

    feat: add dashboard component

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,5 +1,27 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import MainLayout from "@/components/layouts/MainLayout";
 
 export const Dashboard = () => {
-  return <MainLayout>Dashboard</MainLayout>;
+  return (
+    <MainLayout>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem className="hidden md:block">
+            <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="hidden md:block" />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Crypto Fetching</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </MainLayout>
+  );
 };
