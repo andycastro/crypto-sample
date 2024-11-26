1c23573acc13fcf35af92557cb90359f52e9f260
Andy Castro <contato@andycastro.com.br>
2024-11-26 01:33:32 -0300

    feat: improving dashboard view

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -22,6 +22,12 @@ export const Dashboard = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      <div>
+        <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <p className="text-gray-500">
+          Welcome to the Crypto Fetching Dashboard.
+        </p>
+      </div>
     </MainLayout>
   );
 };
