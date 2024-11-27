c841b0ac1f94c5751e9a1025d0774c009485d7e7
Andy Castro <contato@andycastro.com.br>
2024-11-27 11:08:50 -0300

    refactor: avoid unnecessaries request

--- src/hooks/useCryptoData.ts
+++ src/hooks/useCryptoData.ts
@@ -16,36 +16,52 @@ export const useCryptoMarketData = (
     ["cryptoMarketData", cryptoId, currency, days],
     () => getCryptoMarketDataUseCase(cryptoId, currency, days),
     {
-      //refetchInterval: 5000, //voltar para 5 segundos
+      refetchOnWindowFocus: false,
     }
   );
 };
 
 export const useCryptoList = () => {
-  return useQuery("cryptoList", async () => {
-    const cachedCryptoList = localStorage.getItem("cryptoList");
-    if (cachedCryptoList) {
-      return JSON.parse(cachedCryptoList);
-    } else {
-      const cryptoList = await getCryptoListUseCase();
-      localStorage.setItem("cryptoList", JSON.stringify(cryptoList));
-      return cryptoList;
+  return useQuery(
+    "cryptoList",
+    async () => {
+      const cachedCryptoList = localStorage.getItem("cryptoList");
+      if (cachedCryptoList) {
+        return JSON.parse(cachedCryptoList);
+      } else {
+        const cryptoList = await getCryptoListUseCase();
+        localStorage.setItem("cryptoList", JSON.stringify(cryptoList));
+        return cryptoList;
+      }
+    },
+    {
+      refetchOnWindowFocus: false,
     }
-  });
+  );
 };
 
 export const useSupportedVsCurrencies = () => {
-  return useQuery("supportedVsCurrencies", getSupportedVsCurrenciesUseCase);
+  return useQuery("supportedVsCurrencies", getSupportedVsCurrenciesUseCase, {
+    refetchOnWindowFocus: false,
+  });
 };
 
 export const useTopGainingCoins = (currency: string) => {
-  return useQuery(["topGainingCoins", currency], () =>
-    getTopGainingCoinsUseCase(currency)
+  return useQuery(
+    ["topGainingCoins", currency],
+    () => getTopGainingCoinsUseCase(currency),
+    {
+      refetchOnWindowFocus: false,
+    }
   );
 };
 
 export const useMarketCapData = (currency: string) => {
-  return useQuery(["marketCapData", currency], () =>
-    getMarketCapDataUseCase(currency)
+  return useQuery(
+    ["marketCapData", currency],
+    () => getMarketCapDataUseCase(currency),
+    {
+      refetchOnWindowFocus: false,
+    }
   );
 };
